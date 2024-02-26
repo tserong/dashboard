3f4d3b72bdd96c02113f81826004ea5f51aedeb5
Francesco Torchia <francesco.torchia@suse.com>
2024-02-26 15:46:02 +0100

    Add orgId to Addons/rancher-monitoring/Grafana
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- pkg/harvester/edit/harvesterhci.io.addon/rancher-monitoring.vue
+++ pkg/harvester/edit/harvesterhci.io.addon/rancher-monitoring.vue
@@ -168,7 +168,7 @@ export default {
           iconSrc:     grafanaSrc,
           label:       'monitoring.overview.linkedList.grafana.label',
           description: 'monitoring.overview.linkedList.grafana.description',
-          link:        `/k8s/clusters/${ currentCluster.id }/api/v1/namespaces/${ CATTLE_MONITORING_NAMESPACE }/services/http:rancher-monitoring-grafana:80/proxy`,
+          link:        `/k8s/clusters/${ currentCluster.id }/api/v1/namespaces/${ CATTLE_MONITORING_NAMESPACE }/services/http:rancher-monitoring-grafana:80/proxy/?orgId=1`,
         },
         prometheus: {
           enabled:     false,
