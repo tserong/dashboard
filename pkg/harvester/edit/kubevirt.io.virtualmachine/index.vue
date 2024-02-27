62044a9737fe2ac776878ca72beb9ee12b51b0f8
Francesco Torchia <francesco.torchia@suse.com>
2024-02-27 10:12:40 +0100

    Change pc machine-type id
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- pkg/harvester/edit/kubevirt.io.virtualmachine/index.vue
+++ pkg/harvester/edit/kubevirt.io.virtualmachine/index.vue
@@ -108,8 +108,8 @@ export default {
         label: 'q35',
         value: 'q35'
       }, {
-        label: 'pc',
-        value: 'pc'
+        label: 'pc-q35',
+        value: 'pc-q35'
       }];
     },
 
