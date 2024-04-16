7979b48ad1ebd1cbcc463af8d68b55e2f67c5684
Jack Yu <jack.yu@suse.com>
2024-04-17 07:51:51 +0800

    feat: reconnect after disconnecting (#984) (#993)
    Signed-off-by: Jack Yu <jack.yu@suse.com>
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>
    Co-authored-by: Francesco Torchia <francesco.torchia@suse.com>

--- pkg/harvester/components/novnc/NovncConsole.vue
+++ pkg/harvester/components/novnc/NovncConsole.vue
@@ -13,6 +13,15 @@
         </div>
       </main>
     </div>
+    <div v-if="reconnecting">
+      <main class="main-layout">
+        <div class="text-center">
+          <h2 class="text-secondary mt-20">
+            {{ t('vncConsole.reconnecting.message') }}：{{ retryTimes }} of {{ maximumRetryTimes }}
+          </h2>
+        </div>
+      </main>
+    </div>
     <div
       ref="view"
     />
@@ -35,28 +44,75 @@ export default {
 
   data() {
     return {
-      rfb:          null,
-      connected:    false,
-      disconnected: false,
+      rfb:               null,
+      connected:         false,
+      disconnected:      false,
+      reconnectDelay:    3000,
+      reconnecting:      false,
+      maximumRetryTimes: 10,
+      retryTimes:        0,
+      setTimeout:        null,
     };
   },
 
   mounted() {
     this.$nextTick(() => {
-      const rfb = new RFB(this.$refs.view, this.url);
-
-      rfb.addEventListener('connect', () => {
-        this.connected = true;
-      });
-      rfb.addEventListener('disconnect', (e) => {
-        this.disconnected = true;
-      });
-
-      this.rfb = rfb;
+      this.connect();
     });
   },
 
+  beforeDestroy() {
+    this.clearTimeout();
+  },
+
   methods: {
+    connect() {
+      const rfb = new RFB(this.$refs.view, this.url);
+
+      rfb.addEventListener('connect', () => {
+        this.clearTimeout();
+
+        this.connected = true;
+        this.retryTimes = 0;
+        this.reconnecting = false;
+      });
+
+      rfb.addEventListener('disconnect', (e) => {
+        this.clearTimeout();
+
+        this.disconnected = true;
+        this.rfb = null;
+        this.reconnect();
+      });
+
+      this.rfb = rfb;
+    },
+
+    reconnect() {
+      if (this.retryTimes >= this.maximumRetryTimes) {
+        this.reconnecting = false;
+        this.connected = true;
+        this.disconnected = true;
+
+        return;
+      }
+
+      this.retryTimes += 1;
+      this.reconnecting = true;
+      this.connected = false;
+      this.disconnected = false;
+
+      this.setTimeout = setTimeout(() => {
+        this.connect();
+      }, this.reconnectDelay);
+    },
+
+    clearTimeout() {
+      if (this.setTimeout) {
+        clearTimeout(this.setTimeout);
+      }
+    },
+
     disconnect() {
       this.rfb.disconnect();
     },
