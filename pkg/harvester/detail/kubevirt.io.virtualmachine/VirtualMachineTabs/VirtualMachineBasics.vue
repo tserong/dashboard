4c771c63a6471bf0dfe303eb353f0f54fb1e15bb
andy.lee <andy.lee@suse.com>
2024-05-03 17:37:34 +0800

    fix: too long vm name to overlap the console button
    Signed-off-by: andy.lee <andy.lee@suse.com>

--- pkg/harvester/detail/kubevirt.io.virtualmachine/VirtualMachineTabs/VirtualMachineBasics.vue
+++ pkg/harvester/detail/kubevirt.io.virtualmachine/VirtualMachineTabs/VirtualMachineBasics.vue
@@ -136,107 +136,109 @@ export default {
 </script>
 
 <template>
-  <div class="overview-basics">
-    <div class="row">
-      <div class="col span-6">
-        <LabelValue :name="t('harvester.virtualMachine.detail.details.name')" :value="value.nameDisplay">
-          <template #value>
-            <div class="smart-row">
-              <div class="console">
-                {{ value.nameDisplay }} <VMConsoleBar :resource="value" class="consoleBut" />
+  <div>
+    <VMConsoleBar :resource="value" class="consoleBut" />
+    <div class="overview-basics">
+      <div class="row">
+        <div class="col span-6">
+          <LabelValue :name="t('harvester.virtualMachine.detail.details.name')" :value="value.nameDisplay">
+            <template #value>
+              <div class="smart-row">
+                <div class="console">
+                  {{ value.nameDisplay }}
+                </div>
               </div>
-            </div>
-          </template>
-        </LabelValue>
+            </template>
+          </LabelValue>
+        </div>
+        <div class="col span-6">
+          <LabelValue :name="t('harvester.fields.image')" :value="imageName" />
+        </div>
       </div>
 
-      <div class="col span-6">
-        <LabelValue :name="t('harvester.fields.image')" :value="imageName" />
-      </div>
-    </div>
+      <div class="row">
+        <div class="col span-6">
+          <LabelValue :name="t('harvester.virtualMachine.detail.details.hostname')" :value="hostname">
+            <template #value>
+              <div>
+                {{ hostname }}
+              </div>
+            </template>
+          </LabelValue>
+        </div>
 
-    <div class="row">
-      <div class="col span-6">
-        <LabelValue :name="t('harvester.virtualMachine.detail.details.hostname')" :value="hostname">
-          <template #value>
-            <div>
-              {{ hostname }}
-            </div>
-          </template>
-        </LabelValue>
-      </div>
-
-      <div class="col span-6">
-        <LabelValue :name="t('harvester.virtualMachine.detail.details.node')" :value="node">
-          <template #value>
-            <div>
-              {{ node }}
-            </div>
-          </template>
-        </LabelValue>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col span-6">
-        <LabelValue :name="t('harvester.virtualMachine.detail.details.ipAddress')">
-          <template #value>
-            <HarvesterIpAddress v-model="value.id" :row="value" />
-          </template>
-        </LabelValue>
+        <div class="col span-6">
+          <LabelValue :name="t('harvester.virtualMachine.detail.details.node')" :value="node">
+            <template #value>
+              <div>
+                {{ node }}
+              </div>
+            </template>
+          </LabelValue>
+        </div>
       </div>
 
-      <div class="col span-6">
-        <LabelValue :name="t('harvester.virtualMachine.detail.details.created')" :value="creationTimestamp" />
+      <div class="row">
+        <div class="col span-6">
+          <LabelValue :name="t('harvester.virtualMachine.detail.details.ipAddress')">
+            <template #value>
+              <HarvesterIpAddress v-model="value.id" :row="value" />
+            </template>
+          </LabelValue>
+        </div>
+
+        <div class="col span-6">
+          <LabelValue :name="t('harvester.virtualMachine.detail.details.created')" :value="creationTimestamp" />
+        </div>
       </div>
-    </div>
 
-    <hr class="section-divider" />
+      <hr class="section-divider" />
 
-    <h2>{{ t('harvester.virtualMachine.detail.tabs.configurations') }}</h2>
+      <h2>{{ t('harvester.virtualMachine.detail.tabs.configurations') }}</h2>
 
-    <div class="row">
-      <div class="col span-6">
-        <InputOrDisplay :name="t('harvester.virtualMachine.detail.details.bootOrder')" :value="disks" :mode="mode">
-          <template #value>
-            <ul>
-              <li v-for="(disk) in disks" :key="disk.bootOrder">
-                {{ disk.bootOrder }}. {{ disk.name }} ({{ getDeviceType(disk) }})
-              </li>
-            </ul>
-          </template>
-        </InputOrDisplay>
-      </div>
-      <div class="col span-6">
-        <InputOrDisplay :name="t('harvester.virtualMachine.detail.details.CDROMs')" :value="cdroms" :mode="mode">
-          <template #value>
-            <div>
-              <ul v-if="cdroms.length > 0">
-                <li v-for="(rom) in cdroms" :key="rom.name">
-                  {{ rom.name }}
+      <div class="row">
+        <div class="col span-6">
+          <InputOrDisplay :name="t('harvester.virtualMachine.detail.details.bootOrder')" :value="disks" :mode="mode">
+            <template #value>
+              <ul>
+                <li v-for="(disk) in disks" :key="disk.bootOrder">
+                  {{ disk.bootOrder }}. {{ disk.name }} ({{ getDeviceType(disk) }})
                 </li>
               </ul>
-              <span v-else>
-                {{ t("harvester.virtualMachine.detail.notAvailable") }}
-              </span>
-            </div>
-          </template>
-        </InputOrDisplay>
+            </template>
+          </InputOrDisplay>
+        </div>
+        <div class="col span-6">
+          <InputOrDisplay :name="t('harvester.virtualMachine.detail.details.CDROMs')" :value="cdroms" :mode="mode">
+            <template #value>
+              <div>
+                <ul v-if="cdroms.length > 0">
+                  <li v-for="(rom) in cdroms" :key="rom.name">
+                    {{ rom.name }}
+                  </li>
+                </ul>
+                <span v-else>
+                  {{ t("harvester.virtualMachine.detail.notAvailable") }}
+                </span>
+              </div>
+            </template>
+          </InputOrDisplay>
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="col span-6">
-        <LabelValue :name="t('harvester.virtualMachine.detail.details.operatingSystem')" :value="operatingSystem" />
+      <div class="row">
+        <div class="col span-6">
+          <LabelValue :name="t('harvester.virtualMachine.detail.details.operatingSystem')" :value="operatingSystem" />
+        </div>
+        <LabelValue :name="t('harvester.virtualMachine.detail.details.flavor')" :value="flavor" />
       </div>
-      <LabelValue :name="t('harvester.virtualMachine.detail.details.flavor')" :value="flavor" />
-    </div>
-    <div class="row">
-      <div class="col span-6">
-        <LabelValue :name="t('harvester.virtualMachine.detail.details.kernelRelease')" :value="kernelRelease" />
-      </div>
+      <div class="row">
+        <div class="col span-6">
+          <LabelValue :name="t('harvester.virtualMachine.detail.details.kernelRelease')" :value="kernelRelease" />
+        </div>
 
-      <div class="col span-6">
-        <LabelValue :name="t('harvester.virtualMachine.input.MachineType')" :value="machineType" />
+        <div class="col span-6">
+          <LabelValue :name="t('harvester.virtualMachine.input.MachineType')" :value="machineType" />
+        </div>
       </div>
     </div>
   </div>
@@ -244,9 +246,8 @@ export default {
 
 <style lang="scss" scoped>
   .consoleBut {
-    position: relative;
-    top: -20px;
-    left: 38px;
+    display: flex;
+    justify-content: flex-end;
   }
 
   .overview-basics {
@@ -267,6 +268,7 @@ export default {
 
       .console {
         display: flex;
+        overflow: hidden;
       }
     }
 
