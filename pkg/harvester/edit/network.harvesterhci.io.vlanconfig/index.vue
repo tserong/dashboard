17144fcdcdaa8637c0f9f389c504ccfa8ff1e32a
a110605 <andy.lee@suse.com>
2024-07-03 21:58:51 +0800

    fix non-matched rule also show matched node
    Signed-off-by: a110605 <andy.lee@suse.com>
    (cherry picked from commit 2e56ebea77053b2a1aea4c918d4928eedeb5de6c)

--- pkg/harvester/edit/network.harvesterhci.io.vlanconfig/index.vue
+++ pkg/harvester/edit/network.harvesterhci.io.vlanconfig/index.vue
@@ -286,7 +286,6 @@ export default {
         matchNICs = allNICs.filter(n => matchNodes.includes(n.nodeName));
         commonNodes = matchNodes.map(n => n.id);
       }
-
       this.matchNICs = this.intersection(matchNICs, commonNodes) || [];
     }, 250, { leading: true }),
 
@@ -314,12 +313,21 @@ export default {
       } else if (selector[HOSTNAME] && Object.keys(selector).length === 1) {
         const matchNode = allNodes.find(n => n.id === selector[HOSTNAME]);
 
-        this.matchingNodes = {
-          matched: 1,
-          total:   allNodes.length,
-          none:    false,
-          sample:  matchNode ? matchNode.nameDisplay : selector[HOSTNAME],
-        };
+        if (matchNode) {
+          this.matchingNodes = {
+            matched: 1,
+            total:   allNodes.length,
+            none:    false,
+            sample:  matchNode.nameDisplay,
+          };
+        } else {
+          this.matchingNodes = {
+            matched: 0,
+            total:   0,
+            none:    true,
+            sample:  null,
+          };
+        }
       } else {
         const match = matching(allNodes, selector);
 
