0c62a41137647b1d546e14fa9a007e14897b0129
Francesco Torchia <francesco.torchia@suse.com>
2024-07-19 17:21:05 +0200

    Add Lvm storage provisioner settings in Storage Class create page
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

---
+++ pkg/harvester/edit/harvesterhci.io.storage/provisioners/lvm.vue
@@ -0,0 +1,32 @@
+<script>
+
+export default {
+  components: {},
+
+  props: {
+    value: {
+      type:     Object,
+      required: true
+    },
+    mode: {
+      type:     String,
+      required: true
+    },
+    realMode: {
+      type:     String,
+      required: true
+    },
+  },
+
+  data() {
+    return {};
+  },
+
+  computed: {},
+};
+</script>
+<template>
+  <div>
+    LVM settings TODO
+  </div>
+</template>
