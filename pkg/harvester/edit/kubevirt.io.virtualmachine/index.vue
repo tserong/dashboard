e9613b414a849b9b6ad8ba5b1434d363bb615006
andy.lee <andy.lee@suse.com>
2024-09-10 13:30:51 +0800

    Add cancelAction in VM create page

--- pkg/harvester/edit/kubevirt.io.virtualmachine/index.vue
+++ pkg/harvester/edit/kubevirt.io.virtualmachine/index.vue
@@ -270,6 +270,15 @@ export default {
   },
 
   methods: {
+    cancelAction() {
+      const { fromPage = HCI.VM } = this.$route?.query; // default back to VM list page
+      const cancelOverride = {
+        name:   this.doneRoute,
+        params: { resource: fromPage }
+      };
+
+      this.$router.replace(cancelOverride);
+    },
     saveVM(buttonCb) {
       clear(this.errors);
 
@@ -434,12 +443,14 @@ export default {
     id="vm"
     :done-route="doneRoute"
     :resource="value"
+    :cancelEvent="true"
     :mode="mode"
     :can-yaml="isSingle ? true : false"
     :errors="errors"
     :generate-yaml="generateYaml"
     :apply-hooks="applyHooks"
     @finish="saveVM"
+    @cancel="cancelAction"
   >
     <RadioGroup
       v-if="isCreate"
