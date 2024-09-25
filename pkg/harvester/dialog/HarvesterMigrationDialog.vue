1d16e2fe2e8119ead7c122ce8e112bfbbdcce179
andy.lee <andy.lee@suse.com>
2024-09-25 11:19:41 +0800

    add warning banner for cpu pinning VM in migration dialog
    Signed-off-by: andy.lee <andy.lee@suse.com>

--- pkg/harvester/dialog/HarvesterMigrationDialog.vue
+++ pkg/harvester/dialog/HarvesterMigrationDialog.vue
@@ -68,13 +68,17 @@ export default {
       return resource;
     },
 
+    cpuPinningAlertMessage() {
+      return this.t('harvester.virtualMachine.cpuPinning.migrationMessage');
+    },
+
     nodeNameList() {
       const nodes = this.$store.getters['harvester/all'](NODE);
 
       return nodes.filter((n) => {
         const isNotSelfNode = !!this.availableNodes.includes(n.id);
         const isNotWitnessNode = n.isEtcd !== 'true'; // do not allow to migrate to self node and witness node
-        const isCpuPinning = this.actionResource?.spec?.template?.spec?.domain?.cpu?.dedicatedCpuPlacement;
+        const isCpuPinning = this.actionResource?.isCpuPinning;
         const matchingCpuManagerConfig = !isCpuPinning || n.isCPUManagerEnabled; // If cpu-pinning is enabled, filter-out non-enabled CPU manager nodes.
 
         return isNotSelfNode && isNotWitnessNode && matchingCpuManagerConfig;
@@ -144,6 +148,7 @@ export default {
     </template>
 
     <template #body>
+      <Banner v-if="actionResource?.isCpuPinning" color="warning" :label="cpuPinningAlertMessage" />
       <LabeledSelect
         v-model="nodeName"
         :label="t('harvester.modal.migration.fields.nodeName.label')"
@@ -164,7 +169,6 @@ export default {
           @click="apply"
         />
       </div>
-
       <Banner v-for="(err, i) in errors" :key="i" color="error" :label="err" />
     </div>
   </Card>
