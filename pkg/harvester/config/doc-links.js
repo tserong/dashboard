dbacb68c040308a4a0d700220a081bbc4c5e3192
andy.lee <andy.lee@suse.com>
2024-10-18 15:16:24 +0800

    update CONSOLE_URL doc link
    Signed-off-by: andy.lee <andy.lee@suse.com>

--- pkg/harvester/config/doc-links.js
+++ pkg/harvester/config/doc-links.js
@@ -4,7 +4,7 @@ import semver from 'semver';
 const docVersion = `v${ semver.major(version) }.${ semver.minor(version) }`;
 
 export const DOC_LINKS = {
-  CONSOLE_URL:             `https://docs.harvesterhci.io/${ docVersion }/host/`,
+  CONSOLE_URL:             `https://docs.harvesterhci.io/${ docVersion }/host/#remote-console`,
   RANCHER_INTEGRATION_URL: `https://docs.harvesterhci.io/${ docVersion }/rancher/rancher-integration`,
   STORAGE_NETWORK_EXAMPLE: `https://docs.harvesterhci.io/${ docVersion }/advanced/storagenetwork#configuration-example`,
   KSMTUNED_MODE:           `https://docs.harvesterhci.io/${ docVersion }/host/#ksmtuned-mode`,
